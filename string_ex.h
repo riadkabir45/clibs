8ce7c6e16bf43acc710e09e675b21082d9eac0b3
riadkabir45 <riadkabir45@gmail.com>
2020-09-27 22:13:18 +0600

    Removed exxtras

--- string_ex.h
+++ string_ex.h
@@ -9,12 +9,8 @@
 void despace(char arg[]);
 void cleanSpace(char arg[]);
 void revstr(char *text);
-void intstr(int val, char num[]);
-int strint(char num[]);
-bool chkstr(char atext[],char btext[]);
 void strcpyex(char s[],char d[],int start,int dis);
 int strfind(char s[],char t[],int n);
-int sparse(char s[],char t[]);
 
 void despace(char arg[]){
 	int i = 0;
@@ -54,67 +50,6 @@ void revstr(char *text){
 	for(int i = 0;i < n;i++)
 	    text[i] = tmp[n-i-1];
 }
-
-void intstr(int val, char num[]){
-	int n = 0;
-	bool negetive = false;
-	if(val == 0){
-	    num[0] = '0';
-	    num[1] = '\0';
-	    return;
-	}
-	if(val < 0){
-		val = abs(val);
-		negetive = true;
-	}
-	while(val > 0){
-		num[n] = val%10 + '0';
-		val /= 10;
-		n++;
-	}
-	
-	if(negetive){
-		num[n] = '-';
-		n++;
-	}
-	num[n] = '\0';
-	revstr(num);
-}
-
-bool chkstr(char atext[],char btext[]){
-	bool state = true;
-	for(int i = 0;atext[i] != '\0';i++){
-	    for(int j = 0;btext[j] != '\0';j++){
-	    	if(atext[i] == btext[j]){
-	    		state = true;
-	    		break;
-	    	}else
-	    	    state = false;
-	    }
-	    if(!state)
-	    	return false;
-	}
-	return true;
-}
-
-int strint(char num[]){
-	bool negetive = false;
-	if(!chkstr(num,"-0123456789"))
-	    return 0;
-	int val = 0;
-	if(num[0] == '-'){
-		negetive = true;
-	}
-	strcpyex(num,num,1,strlen(num)-1);
-	for(int i = 0;i < strlen(num);i++){
-	    val *= 10;
-	    val += num[i]-'0';
-	}
-	if(negetive)
-		val = 0- val;
-	return val;
-}
-
 
 void strcpyex(char s[],char d[],int start,int dis){
 	if(dis < 1)
